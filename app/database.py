397d4ec8eb73b0ed4a215de726c131ba40920e32
Alejandro <voidreamer@gmail.com>
2024-12-27 11:43:43 -0500

    update key

--- app/database.py
+++ app/database.py
@@ -7,17 +7,8 @@ from dotenv import load_dotenv
 # Load environment variables from the .env file
 load_dotenv()
 
-# Retrieve Supabase connection details from environment variables
-SUPABASE_PROJECT_URL = os.getenv("SUPABASE_PROJECT_URL")
-SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
-
-# Construct the SQLAlchemy database URL for Supabase
-SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:{SUPABASE_SERVICE_ROLE_KEY}@{SUPABASE_PROJECT_URL.split('//')[1]}/postgres"
-
 # Create the SQLAlchemy engine
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
+engine = create_engine(os.getenv('DATABASE_URL'))
 
 # Create a configured "SessionLocal" class
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
