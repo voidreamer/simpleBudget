77aee733e592d98c1d4f5da3dad364424ff3fb73
Alejandro <voidreamer@gmail.com>
2024-12-27 10:43:59 -0500

    change db place to postgres

--- app/database.py
+++ app/database.py
@@ -1,12 +1,26 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./budget.db"
+# Load environment variables from the .env file
+load_dotenv()
 
+# Retrieve Supabase connection details from environment variables
+SUPABASE_PROJECT_URL = os.getenv("SUPABASE_PROJECT_URL")
+SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
+
+# Construct the SQLAlchemy database URL for Supabase
+SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:{SUPABASE_SERVICE_ROLE_KEY}@{SUPABASE_PROJECT_URL.split('//')[1]}/postgres"
+
+# Create the SQLAlchemy engine
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
+
+# Create a configured "SessionLocal" class
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Base class for models
 Base = declarative_base()
