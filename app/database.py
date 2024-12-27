677ba4061b9779cb37d24d00a6e725928a951948
Alejandro <voidreamer@gmail.com>
2024-12-27 11:57:00 -0500

    update key

--- app/database.py
+++ app/database.py
@@ -1,5 +1,6 @@
 import os
 from sqlalchemy import create_engine
+from sqlalchemy import NullPool
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
@@ -8,7 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Create the SQLAlchemy engine
-engine = create_engine(os.getenv('DATABASE_URL'))
+engine = create_engine(os.getenv('DATABASE_URL'), client_encoding='utf8', poolclass=NullPool)
 
 # Create a configured "SessionLocal" class
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
